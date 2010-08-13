ee5ca898e823d0f0e97a159393805b2931aaf9dc
Bret Weinraub <bret@ckuru.com>
2010-08-13 17:52:11 +0000

    move version to 0.0.2

--- tasks/setup.rb
+++ tasks/setup.rb
@@ -17,7 +17,7 @@ PROJ = OpenStruct.new(
   :authors => nil,
   :email => nil,
   :url => "\000",
-  :version => ENV['VERSION'] || '0.0.0',
+  :version => ENV['VERSION'] || '0.0.2',
   :exclude => %w(tmp$ bak$ ~$ CVS \.svn/ \.git/ ^pkg/),
   :release_name => ENV['RELEASE'],
 
